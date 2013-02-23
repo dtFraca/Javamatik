f2c6e19ae720db90eb643618a69cc72740c4d847
David <david@FIBA>
2013-02-23 23:46:52 +0100

    Ajoute module 93_DavidEntrainement

---
+++ 93_DavidEntrainement/src/EchoFrappe.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Class Description: ?
+ * <p/>
+ * 2013-02-23 - david
+ */
+public class EchoFrappe {
+    public static void main(String[] args) {
+        System.out.print("Veuillez entrez votre nom: ");
+        Scanner myScan = new Scanner(System.in);
+        String username = myScan.nextLine();
+        System.out.printf("Votre nom est............: %s", username);
+
+
+    }
+}
