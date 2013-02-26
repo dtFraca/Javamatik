caf6ff8f92a37f0cc5708307f88257ad051860f9
David <david@FIBA>
2013-02-26 20:27:38 +0100

    Ajoute Exo_2a et 2c

---
+++ 01_ExoJava/src/dtfraca/Exo02_DatatypeAndOperator/CalculDecimal.java
@@ -0,0 +1,19 @@
+package dtfraca.Exo02_DatatypeAndOperator;
+
+/**
+ * Class Description: ?
+ * <p/>
+ * 2013-02-26 - david
+ */
+public class CalculDecimal {
+    public static void main(String[] args) {
+        float f1 = 11f;
+        float f2 = 3f;
+
+        System.out.printf("Résultat type float.................................: %f / %f = %f\n", f1, f2, f1/f2);
+        System.out.printf("Résultat type float (deux chiffres après la virgule): %.2f / %.2f = %.2f\n", f1, f2, f1/f2);
+
+        System.out.printf("Racine carré de 50: %.5f\n", Math.sqrt(50d));// Sqrt = Square root
+        System.out.printf("Valeur de Pi: %.5f\n", Math.PI);
+    }
+}
