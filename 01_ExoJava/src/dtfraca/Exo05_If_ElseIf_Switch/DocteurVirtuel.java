626b90bd195ccf83f7c328a2d9541c0828c86b36
David <david@FIBA>
2013-03-20 00:08:16 +0100

    + DocteurVirtuel, + LesMathsDuPauvre

---
+++ 01_ExoJava/src/dtfraca/Exo05_If_ElseIf_Switch/DocteurVirtuel.java
@@ -0,0 +1,44 @@
+package dtfraca.Exo05_If_ElseIf_Switch;
+
+import java.util.Scanner;
+
+/**
+ * Apprendre structure IF...ELSE IF...ELSE
+ * <p/>
+ * 2013-03-19 - david
+ */
+public class DocteurVirtuel {
+	public static void main(String[] args) {
+
+		Scanner myScan = new Scanner(System.in);
+		System.out.print("Veuillez entrez vos symptomes: ");
+		String descripMaladie = myScan.nextLine().toUpperCase();
+		String msgDocteur;
+
+		if (descripMaladie.contains("TOUSSE") && descripMaladie.contains("SANG")) {
+			msgDocteur = "Tuberculose";
+		}
+		else if (descripMaladie.contains("TOUSSE") && descripMaladie.contains("GORGE")) {
+			msgDocteur = "Angine aigue";
+		}
+		else if (descripMaladie.contains("GORGE")) {
+			msgDocteur = "Angine";
+		}
+		else if (descripMaladie.contains("TETE")) {
+			msgDocteur = "Migraine";
+		}
+		else if (descripMaladie.contains("FATIGUE")) {
+			msgDocteur = "Besoin d'exercices";
+		}
+		else if (descripMaladie.contains("TOUSSE")) {
+			msgDocteur = "Toux sèche";
+		}
+
+		else {
+			msgDocteur = "Maladie inconnue";
+		}
+		System.out.printf("Votre maladie: %s\n", msgDocteur);
+
+
+	}
+}
---
+++ 01_ExoJava/src/dtfraca/Exo05_If_ElseIf_Switch/LesMathsDuPauvre.java
@@ -0,0 +1,60 @@
+package dtfraca.Exo05_If_ElseIf_Switch;
+
+import java.util.Scanner;
+
+/**
+ * Class Description: ?
+ * <p/>
+ * 2013-03-19 - david
+ */
+public class LesMathsDuPauvre {
+	public static void main(String[] args) {
+
+		System.out.println("1. Calculer hypothénuse");
+		System.out.println("2. Surface d’un cercle");
+		System.out.println("3. Circonference d’un cercle");
+		System.out.println("4. Convertir un nombre entier en binaire");
+		System.out.println("5. 2 a la puissance N");
+		System.out.print("VOTRE CHOIX: ");
+		Scanner myScan = new Scanner(System.in);
+		int choixMenu = myScan.nextInt();
+
+
+		Scanner paramCalcul = new Scanner(System.in);
+
+		switch (choixMenu) {
+			case 1:
+				System.out.print("Hypothénuse, donner valeur des deux petits côtés: ");
+				double cote1 = paramCalcul.nextDouble();
+				double cote2 = paramCalcul.nextDouble();
+				double hypothenuse = Math.hypot(cote1, cote2);
+				System.out.printf("c1 = %.2f, c2 = %.2f, Hypothénuse = %.2f\n", cote1, cote2, hypothenuse);
+				break;
+
+			case 2:
+				System.out.print("Surface, donner valeur rayon: ");
+				double rayon1 = paramCalcul.nextDouble();
+				double surface = Math.PI * Math.pow(rayon1, 2);//rayon1 * rayon1;
+				System.out.printf("Rayon = %.2f, Surface = %.2f", rayon1, surface);
+				break;
+
+			case 3:
+				System.out.print("Circonference, donner valeur rayon: ");
+				break;
+
+			case 4:
+				System.out.print("Conversion binaire, donner valeur décimal entière: ");
+				break;
+
+			case 5:
+				System.out.print("2^N, donner valeur puissance N: ");
+				break;
+
+
+			default:
+				System.out.print("Choix inconnu");
+				break;
+		}
+
+	}
+}
