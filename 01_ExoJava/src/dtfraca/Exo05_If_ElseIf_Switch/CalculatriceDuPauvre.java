37d701b661d88f1c3a3244089147c1e463bcc885
Trinosaur <tri@LHC>
2013-04-04 16:00:14 -0400

    Corrige CalculatriceDuPauvre (a refaire), propose variante pour InspecteurSystem

--- 01_ExoJava/src/dtfraca/Exo05_If_ElseIf_Switch/CalculatriceDuPauvre.java
+++ 01_ExoJava/src/dtfraca/Exo05_If_ElseIf_Switch/CalculatriceDuPauvre.java
@@ -9,49 +9,15 @@ import java.util.Scanner;
  */
 public class CalculatriceDuPauvre {
 	public static void main(String[] args) {
-		System.out.println("1. Multiplication");
-		System.out.println("2. Addition");
-		System.out.println("3. Soustraction");
-		System.out.println("4. Division");
-		System.out.print("VOTRE CHOIX: ");
-		Scanner myScan = new Scanner(System.in);
-		Scanner paramCalcul = new Scanner(System.in);
-		int choixMenu = myScan.nextInt();
+		System.out.print("Veuillez entrer l'expression de calcul: "); // Ex: "13 + 14.5"  ou "2.7 * -3.5"
+		Scanner scanExpCalc = new Scanner(System.in);
 
-		switch (choixMenu) {
-			case 1:
-				System.out.print("Veuillez entrez deux nombres pour les multipliés: ");
-				double nombreMulti1 = paramCalcul.nextDouble();
-				double nombreMulti2 = paramCalcul.nextDouble();
-				System.out.printf("%.2f x %.2f = %.2f", nombreMulti1, nombreMulti2, nombreMulti1 * nombreMulti2);
-				break;
-
-			case 2:
-				System.out.print("Veuillez entrez deux nombres pour les additionés: ");
-				double nombreAdi1  = paramCalcul.nextDouble();
-				double nombreAdi2 = paramCalcul.nextDouble();
-				System.out.printf("%.2f + %.2f = %.2f", nombreAdi1, nombreAdi2, nombreAdi1 + nombreAdi2);
-				break;
-
-			case 3:
-				System.out.print("Veuillez entrez deux nombres pour les soustraires: ");
-				double nombreSous1 = paramCalcul.nextDouble();
-				double nombreSous2 = paramCalcul.nextDouble();
-				System.out.printf("%.2f - %.2f = %.2f", nombreSous1,nombreSous2, nombreSous1 - nombreSous2);
-				break;
-
-			case 4:
-				System.out.print("Veuillez entrez deux nombres pour les divisés: ");
-				double nombreDivi1 = paramCalcul.nextDouble();
-				double nombreDivi2 = paramCalcul.nextDouble();
-				System.out.printf("%.2f : %.2f = %.2f", nombreDivi1, nombreDivi2, nombreDivi1 / nombreDivi2);
-				break;
-
-			default:
-				System.out.print("Choix inconnu");
-				break;
+		double n1 = scanExpCalc.nextDouble();
+		String symbolOperateur = scanExpCalc.next();
+		double n2 = scanExpCalc.nextDouble();
+		System.out.printf("n1 = %.2f\nOperateur = %s\nn2 = %.2f\n", n1, symbolOperateur, n2);
 
 	}
-  }
 }
+
 
--- 01_ExoJava/src/dtfraca/Exo05_If_ElseIf_Switch/InspecteurSystem.java
+++ 01_ExoJava/src/dtfraca/Exo05_If_ElseIf_Switch/InspecteurSystem.java
@@ -13,22 +13,36 @@ public class InspecteurSystem {
 
 		Properties prop = System.getProperties();
 		Enumeration keys = prop.keys();
+		boolean isWindowsOS = false;
+
 		while (keys.hasMoreElements()) {
 			String key = (String) keys.nextElement();
 			String value = (String) prop.get(key);
 			System.out.printf("%s = %s\n", key, value);
 
-
+			if (key.equals("os.name")) {
+				if (value.toLowerCase().contains("windows")) {
+					isWindowsOS = true;
+				}
+			}
 		}
-		System.out.println();
+
 		System.out.println();
 		System.out.println();
 
+		if (isWindowsOS) {
+			System.out.println("==== Vous avez Windows, happy with Activation?");
+		}
+		else {
+			System.out.printf("==== Vous n'avez pas Windows, votre OS est: %s\n", prop.get("os.name"));
+		}
+
+
+		// version plus simple, utilisant recherche de sous-chaine d'une valeur dans une collection
+		// inconvenient: tous les elements sont testes, et pas uniquement la cle "os.name"
 		if (prop.contains("Linux")) {
 			System.out.println("C'est bien vous avez Linux");
-
 		}
-
 		else {
 			System.out.println("Il est temps d'apprendre Linux");
 		}
